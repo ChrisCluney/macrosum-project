4a2097fa69d32bee237bc5f835ce696a04a2879c
ChrisCluney <ccluney@gmail.com>
2022-03-10 15:29:43 -0600

    Addition implementation working

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const app = express();
 
 app.use(cors());
 
+const path = require("path");
+
 app.use(express.json()); // When we want to be able to accept JSON.
 
 const axios = require("axios");
@@ -20,4 +22,4 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "../index.html"));
 });
 
-app.listen(port, () => console.log("Server running on 5000"));
+app.listen(port, () => console.log(`Server running on ${port}`));
